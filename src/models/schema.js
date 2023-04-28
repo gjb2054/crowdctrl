d299d170d3a74ad394ed10837f7fbb2ea3c2303e
gjb2054 <gjb2054@rit.edu>
2023-04-28 00:02:30 -0400

    Codegen

--- src/models/schema.js
+++ src/models/schema.js
@@ -98,18 +98,6 @@ export const schema = {
                                     "delete",
                                     "read"
                                 ]
-                            },
-                            {
-                                "provider": "userPools",
-                                "ownerField": "owner",
-                                "allow": "owner",
-                                "identityClaim": "cognito:username",
-                                "operations": [
-                                    "create",
-                                    "update",
-                                    "delete",
-                                    "read"
-                                ]
                             }
                         ]
                     }
