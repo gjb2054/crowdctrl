61d93b25d0aa80eb32f0d4ffd0274a7dc53d5908
gjb2054 <gjb2054@rit.edu>
2023-04-28 00:42:14 -0400

    Graphql Test

--- src/models/schema.js
+++ src/models/schema.js
@@ -98,6 +98,18 @@ export const schema = {
                                     "delete",
                                     "read"
                                 ]
+                            },
+                            {
+                                "provider": "userPools",
+                                "ownerField": "owner",
+                                "allow": "owner",
+                                "identityClaim": "cognito:username",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
                             }
                         ]
                     }
